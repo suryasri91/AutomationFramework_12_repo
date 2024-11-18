ef8debc599378e8a352c8d174c32615e9b5a1774
aparn <aparn@Aparna>
2024-11-18 12:45:01 +0530

    SyncFork class added

---
+++ AutomationFramework/src/test/java/aparna/syncFork.java
@@ -0,0 +1,12 @@
+package aparna;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class syncFork {
+	@Test
+	public void sync() {
+		Reporter.log("SyncFork class added", true);
+	}
+
+}
