c2416c84ad6df0a677f73b6b8c15c659e5003a2f
suryasri91 <mccheerssurya007@gmail.com>
2024-11-20 13:55:44 +0530

    pollscm executed

---
+++ AutomationFramework/src/test/java/pollscm/PollSCMTest.java
@@ -0,0 +1,11 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCMTest {
+	@Test
+	public void scm() {
+		Reporter.log("pollscm class executed", true);
+	}
+}
